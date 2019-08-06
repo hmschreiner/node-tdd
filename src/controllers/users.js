dc30f78deca151e65ee6806b573db87a4193cc11
Henrique Schreiner <hms.sud@gmail.com>
2019-08-06 20:21:29 -0300

    Adding green cicle

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,5 +1,17 @@
 class UsersController {
-  
+  constructor (User) {
+    this.User = User
+  }
+
+  async findAll (req, res) {
+
+    res.send([{
+      id: '56cb91bdc3464f14678934ca',
+      firstName: 'Default',
+      lastName: 'User',
+      email: 'user@mail.com'
+    }])
+  }
 }
 
 export default UsersController
